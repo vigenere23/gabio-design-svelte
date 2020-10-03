96f17417d0d91cd92d39b86db11f2943e17703ea
vigenere23 <lolgab1@hotmail.com>
2020-10-03 10:12:19 -0400

    added better rem calculator

--- src/components/typography/GioTitle.svelte
+++ src/components/typography/GioTitle.svelte
@@ -1,9 +1,4 @@
-<h1
-  class="gio-title"
-  class:noMargin
-  class:dark
-  class:centered
-  style="font-size: {fontSize}">
+<h1 class="gio-title" class:noMargin class:dark class:centered>
   <slot />
 </h1>
 
@@ -11,20 +6,24 @@
   export let noMargin = false
   export let dark = false
   export let centered = false
-  export let fontSize = 14 * 4 + 'px'
 </script>
 
 <style lang="scss">
   @import '../../styles/fonts.scss';
   @import '../../styles/colors.scss';
+  @import '../../styles/sizes.scss';
 
   .gio-title {
     font-family: $special-font;
     font-weight: 700;
     color: $primary-text-dark;
+    margin-top: rem(32px);
+    margin-bottom: rem(32px);
+    font-size: rem(56px);
 
-    &:not(.no-margin) {
-      margin: 8rem 0;
+    &.noMargin {
+      margin-top: 0;
+      margin-bottom: 0;
     }
 
     &.centered {
