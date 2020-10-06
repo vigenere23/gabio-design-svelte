f43493ac2d1c91ba8de6e3604931aaf99daf1983
vigenere23 <lolgab1@hotmail.com>
2020-10-06 19:30:41 -0400

    added lists

---
+++ src/components/typography/GioList.svelte
@@ -0,0 +1,38 @@
+<ul class="gio-list" class:indent class:noMargin>
+  <slot />
+</ul>
+
+<script lang="ts">
+  export let indent = false
+  export let noMargin = false
+</script>
+
+<style lang="scss">
+  @import '../../styles/sizes';
+
+  .gio-list {
+    line-height: 1.75em;
+    list-style: none;
+    padding: 0;
+    margin-left: 0;
+    margin-right: 0;
+    @include text-margin;
+
+    &.noMargin {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+
+    &.indent {
+      margin-left: rem(24px);
+    }
+
+    :global(.gio-list) {
+      margin: 0;
+
+      &.indent {
+        margin-left: 0;
+      }
+    }
+  }
+</style>
---
+++ src/components/typography/GioListItem.svelte
@@ -0,0 +1,17 @@
+<li class="gio-list-item">
+  <slot />
+</li>
+
+<style lang="scss">
+  .gio-list-item {
+    padding-left: 1.5em;
+    line-height: inherit;
+
+    &::before {
+      content: '\279C'; // HEAVY ROUND-TIPPED RIGHT ARROW
+      position: absolute;
+      left: 0.25em;
+      color: inherit;
+    }
+  }
+</style>
