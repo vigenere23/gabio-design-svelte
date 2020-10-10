cf6491069a8464f279d0bf95c5542de78e13a052
vigenere23 <lolgab1@hotmail.com>
2020-10-10 15:10:11 -0400

    replaced object with map

--- src/lib/icon-registry.ts
+++ src/lib/icon-registry.ts
@@ -5,28 +5,25 @@ import type {
 import { fromPascalCaseToKebabCase } from './strings'
 
 type IconsInput = { [key: string]: IconDefinition }
-type Icons = {
-  [key in IconName]?: IconDefinition
-}
 
 export class IconRegistry {
-  private static icons: Icons = {}
+  private static icons: Map<IconName, IconDefinition> = new Map()
 
   public static registerIcons(icons: IconsInput) {
     for (const [name, icon] of Object.entries(icons)) {
       const iconName = transformIconName(name)
-      IconRegistry.icons[iconName] = icon
+      IconRegistry.icons.set(iconName, icon)
     }
   }
 
   public static getIcon(iconName: IconName): IconDefinition {
-    const icon = IconRegistry.icons[iconName]
+    const icon = IconRegistry.icons.get(iconName)
     if (!icon) {
       throw new Error(
         `Icon ${iconName} was not found. Did you register it with 'IconRegistry.registerIcons()'?`
       )
     }
-    return icon as IconDefinition
+    return icon
   }
 }
 
