994014acd5d939aa90f9618017e8e7465a23475e
vigenere23 <lolgab1@hotmail.com>
2020-10-24 15:28:57 -0400

    added dynamic dependencies top markdown transpiler + updated dependencies

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,22 +1,38 @@
-import type { MarkdownParser } from '@gabio/markdown-transpiler'
+import type {
+  MarkdownParser,
+  MarkdownParserOutput
+} from '@gabio/markdown-transpiler'
 import marked, { Renderer } from 'marked'
 
 class GioSvelteMarkdownRenderer extends Renderer {
+  private components: Set<string> = new Set()
+
   heading(text: string, level: number): string {
     const id = text.toLowerCase().replace(/[^\w]+/g, '-')
-    return level === 1
-      ? `<GioTitle noMargin fontSize="${48 / 16}rem">${text}</GioTitle>\n`
-      : level === 6
-      ? `<GioSubtitle>${text}</GioSubtitle>\n`
-      : `<GioHeading level={${level - 1}} id="#${id}">${text}</GioHeading>\n`
+
+    if (level === 1) {
+      this.addComponentDependencies(['GioTitle'])
+      return `<GioTitle noMargin fontSize="${48 / 16}rem">${text}</GioTitle>\n`
+    } else if (level === 6) {
+      this.addComponentDependencies(['GioSubtitle'])
+      return `<GioSubtitle>${text}</GioSubtitle>\n`
+    } else {
+      this.addComponentDependencies(['GioHeading'])
+      return `<GioHeading level={${
+        level - 1
+      }} id="#${id}">${text}</GioHeading>\n`
+    }
   }
 
   paragraph(text: string): string {
+    this.addComponentDependencies(['GioBodyText'])
     return `<GioBodyText>${text}</GioBodyText>\n`
   }
 
   image(href: string, title: string, text: string): string {
     const caption = text || title || ''
+    this.addComponentDependencies(['GioCaptionedImage', 'GioImage'])
+
     return `
       <GioCaptionedImage caption="${caption}">
         <div slot="image">
@@ -30,60 +46,58 @@ class GioSvelteMarkdownRenderer extends Renderer {
   }
 
   blockquote(quote: string): string {
+    this.addComponentDependencies(['GioCaption'])
     return `<GioCaption>${quote}</GioCaption>\n`
   }
 
   codespan(code: string): string {
     code = this.svelteSafe(code)
+    this.addComponentDependencies(['GioInlineCode'])
     return `<GioInlineCode>${code}</GioInlineCode>`
   }
 
   code(code: string, language?: string): string {
+    this.addComponentDependencies(['GioCodeBlock'])
     return `<GioCodeBlock language="${language}" code="${encodeURI(code)}" />\n`
   }
 
   list(body: string /*ordered: boolean, start: number*/): string {
+    this.addComponentDependencies(['GioBodyText', 'GioList'])
     return `<GioBodyText noMargin><GioList indent>${body}</GioList></GioBodyText>\n`
   }
 
   listitem(text: string): string {
+    this.addComponentDependencies(['GioListItem'])
     return `<GioListItem>${text}</GioListItem>\n`
   }
 
   link(href: string, _title: string, text: string) {
+    this.addComponentDependencies(['GioSmartLink'])
     return `<GioSmartLink accent href="${href}">${text}</GioSmartLink>`
+  }
+
+  getComponents(): string[] {
+    return Array.from(this.components.values())
   }
 
   private svelteSafe(text: string): string {
     return text.replace(/{/g, '&lbrace;').replace(/}/g, '&rbrace;')
   }
+
+  private addComponentDependencies(components: string[]) {
+    components.forEach((component) => this.components.add(component))
+  }
 }
 
 export class GioSvelteMarkdownParser implements MarkdownParser {
-  private renderer: Renderer = new GioSvelteMarkdownRenderer()
+  parse(markdownContent: string): MarkdownParserOutput {
+    const renderer = new GioSvelteMarkdownRenderer()
 
-  getDependencies() {
     return {
-      '@gabio/design-svelte': [
-        'GioBodyText',
-        'GioTitle',
-        'GioSubtitle',
-        'GioHeading',
-        'GioCaption',
-        'GioInlineCode',
-        'GioCodeBlock',
-        'GioList',
-        'GioListItem',
-        'GioCaptionedImage',
-        'GioSmartLink',
-        'GioImage'
-      ]
+      parsedContent: marked(markdownContent, {
+        renderer
+      }),
+      dependencies: { '@gabio/design-svelte': renderer.getComponents() }
     }
-  }
-
-  parse(markdownContent: string): string {
-    return marked(markdownContent, {
-      renderer: this.renderer
-    })
   }
 }
